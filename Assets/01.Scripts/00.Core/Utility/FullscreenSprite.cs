0a3e42258ca2722271888ae94d64fd570e62d0be
Jae Yeop Lee <myidmm64@gmail.com>
2024-08-06 03:15:25 +0900

    0806_임시 배경 스프라이트 적용

---
+++ Assets/01.Scripts/00.Core/Utility/FullscreenSprite.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class FullscreenSprite : MonoBehaviour
+{
+    [SerializeField]
+    private bool _positionReset = false;
+
+    void Awake()
+    {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+
+        float cameraHeight = Camera.main.orthographicSize * 2;
+        Vector2 cameraSize = new Vector2(Camera.main.aspect * cameraHeight, cameraHeight);
+        Vector2 spriteSize = spriteRenderer.sprite.bounds.size;
+
+        Vector2 scale = transform.localScale;
+        if (cameraSize.x >= cameraSize.y)
+        { // Landscape (or equal)
+            scale *= cameraSize.x / spriteSize.x;
+        }
+        else
+        { // Portrait
+            scale *= cameraSize.y / spriteSize.y;
+        }
+
+        if(_positionReset)
+            transform.position = Vector2.zero; // Optional
+        transform.localScale = scale;
+    }
+}
