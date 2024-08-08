1d3926da318a80c66e29f4b8fec5003f02bd1d76
Jae Yeop Lee <myidmm64@gmail.com>
2024-08-08 23:23:49 +0900

    0808_skill 만들다 만 코드

--- Assets/01.Scripts/DiceUnit/Player/PlayerSkill.cs
+++ Assets/01.Scripts/DiceUnit/Player/PlayerSkill.cs
@@ -2,15 +2,12 @@ using UnityEngine;
 
 public class PlayerSkill : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public bool IsMPSkill = false; // mp를 사용하는 스킬인지
+    public int maxMP = 100; // 해당 스킬을 사용하기 위해 필요한 mp는 무엇인지
+    private PlayerSkill _skill = null;
 
-    // Update is called once per frame
-    void Update()
+    public void UseSkill()
     {
-        
+
     }
 }
