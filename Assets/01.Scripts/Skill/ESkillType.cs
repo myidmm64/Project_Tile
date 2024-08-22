ae1b18264ecff03a673cd3e6e4f7dfd140c80e43
Jae Yeop Lee <myidmm64@gmail.com>
2024-08-22 21:44:03 +0900

    0822_스킬 약간 추가

---
+++ Assets/01.Scripts/Skill/ESkillType.cs
@@ -0,0 +1,7 @@
+public enum ESkillType
+{
+    None,
+    EnemyPattern,
+    PlayerMain,
+    PlayerSpecial,
+}
--- Assets/01.Scripts/Skill/SSkillData.cs
+++ Assets/01.Scripts/Skill/SSkillData.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [System.Serializable]
 public struct SSkillData
 {
+    public ESkillType skillType;
     public int maxMP;
     [TextArea]
     public string rangePattern;
